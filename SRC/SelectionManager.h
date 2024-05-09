bb1659592545a82091685f53274bded813f53bdd
Espi <daniel.alonsoherranz@gmail.com>
2024-05-09 23:47:30 +0200

    Eliminación de variables sin usar

--- SRC/SelectionManager.h
+++ SRC/SelectionManager.h
@@ -40,7 +40,6 @@ private:
 	int player2Selection;
 	bool confirmation;
 
-	std::string text;
 	std::vector<std::string> materials;
 	std::vector<std::string> maps;
 
--- bin/Assets/scenes.forge.lua
+++ bin/Assets/scenes.forge.lua
@@ -22,7 +22,7 @@ local prefabs = {
                 Mesh={
                     mesh ="Bola.mesh",
                     material= "MatPePa"
-                 }
+            }
         }
     },
     
